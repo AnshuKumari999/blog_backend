a1ed1b0be377bc0c6a73b7bee3b999f1443b4c38
AnshuKumari <91437428+AnshuKumari999@users.noreply.github.com>
2022-09-28 15:03:41 +0530

    Initial commit

---
+++ src/pages/Hollywood.jsx
@@ -0,0 +1,65 @@
+// import React, { useContext } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+// import { store } from "./Details";
+import Card from "../component/Card";
+import SmallCard from "../component/SmallCard";
+
+const Hollywood = () => {
+  // const [detail, setDetail] = useContext(store);
+  // console.log(detail);
+  const [detail, setDetail] = useState([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      const data = await axios.get("http://localhost:8080/");
+      setDetail(data.data);
+    };
+    getData();
+  }, []);
+
+  return (
+    <div>
+      <h1 style={{ margin: "20px 10%", display: "inline-block" }}>Hollywood</h1>
+      <h1 style={{ margin: "20px 0px 20px 35%", display: "inline-block" }}>
+        Top Posts
+      </h1>
+      <div className="main_container">
+        <div className="rightbar">
+          {detail
+            .filter((article) => {
+              return article.category === "Hollywood";
+            })
+            .map((n) => (
+              <Card
+                articleid={n.id}
+                imgUrl={n.Image}
+                title={n.title}
+                description={n.description.slice(0, 200)}
+              />
+            ))}
+        </div>
+
+        <div className="sidebar">
+          {detail
+            .filter((article) => {
+              return article.category === "Hollywood";
+            })
+            .map((n) => (
+              <SmallCard
+                articleid={n.id}
+                imgUrl={n.Image}
+                description={n.description.slice(0, 200)}
+                title={n.title.slice(0, 25)}
+              />
+            ))}
+          <div className="advertisement">
+            <p>Advertisement</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Hollywood;
